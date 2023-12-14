47efb73a6b06dc85d101c554f40647451022d568
nicolasmuller01 <nicolasmatiasmuller19@gmail.com>
2023-12-14 16:39:30 -0300

    deploy fixing 1

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,7 +1,7 @@
 import RegisterForm from "@/components/RegisterForm";
 import React from "react";
 
-export const Page = () => {
+const Page = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <RegisterForm />
